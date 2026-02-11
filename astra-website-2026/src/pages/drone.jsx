2ad19a72d1a744393299728725353656a0919d9e
JustinDrakeSantos <justin.k.santos130@gmail.com>
2026-02-10 18:33:19 -0800

    Home Page and Navbar Implemented (Search Logic Not Complete)

---
+++ astra-website-2026/src/pages/drone.jsx
@@ -0,0 +1,10 @@
+function Drone () {
+    return (
+        <>
+            <h1>Drone Page</h1>
+        </>
+    );
+
+}
+
+export default Drone;
---
+++ astra-website-2026/src/pages/team.jsx
@@ -0,0 +1,8 @@
+function Team() {
+    return (
+    <>
+        <h1>Team Page</h1>
+    </>);
+}
+
+export default Team;
